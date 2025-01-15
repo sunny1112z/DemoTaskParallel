277c697781a54df9ece0366bb62cab5f0d949dda
sunny1112w <duyn2280z@gmail.com>
2025-01-15 14:43:33 +0700

    Add project files.

---
+++ DemoTaskParallel/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DemoTaskParallel
+{
+    internal class Program
+    {
+        public static void PrintNumnber(string message)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($" {message} : {i}");
+                Thread.Sleep(1000);
+            }
+        }//end PrintNumber
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+            //create a task by using a lambda expression
+            Task task01 = new Task(() => PrintNumnber("Task 01"));
+            task01.Start();
+            //Create a task by using delegate and run the task 
+            Task task02 = Task.Run(delegate { PrintNumnber("Task 02"); });
+            //create a task by using a Action delegate
+            Task task03 = new Task(new Action(() => { PrintNumnber("Task 03"); }));
+            task03.Start();
+            Console.WriteLine($"Thread '{Thread.CurrentThread.Name}'");
+            Console.ReadKey();
+        }//end Main
+    }//end Program
+}
