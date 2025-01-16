fe5bd4b1cd2854888c5f1286e3d37e02c0d181ff
sunny1112w <duyn2280z@gmail.com>
2025-01-16 11:57:57 +0700

    16/01/2025

---
+++ DemoTaskParallel2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DemoTaskParallel2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wait for all tasks to comple
+            Task[] tasks = new Task[5];
+            String taskData = "Hello";
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Task.Run(() => { Console.WriteLine($"Task = {Task.CurrentId} , obj = {taskData}" + $"ThreadId = {Thread.CurrentThread.ManagedThreadId}"); });
+                Thread.Sleep(1000);
+            }
+            try
+            {
+             Task.WaitAll(tasks);
+            }
+            catch (AggregateException ae)
+            {
+
+                Console.WriteLine("One or more exceptions occurred :");
+                foreach (var ex in ae.Flatten().InnerExceptions)
+                    Console.WriteLine(" {0}",ex.Message);
+            }
+            Console.WriteLine("Status of completed tasks :");
+            foreach (var t in tasks)
+            {
+                Console.WriteLine(" Task #{0} :{1}", t.Id,t.Status);
+            }
+        }
+    }
+}
